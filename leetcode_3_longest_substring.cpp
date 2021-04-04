f50517d4ad03ca687eb54fb856ffe5308acae354
Jochen Issing <c.333+github@nesono.com>
2021-04-04 22:40:05 +0200

    Solve longest substring problem

--- leetcode_3_longest_substring.cpp
+++ leetcode_3_longest_substring.cpp
@@ -12,24 +12,32 @@ public:
             return 0;
         }
 
-        std::unordered_set<char> seenChars;
-        int result = 0;
+        size_t left = 0;
+        size_t result = 1;
+        size_t count = 1;
 
-        for (const auto& c : s) {
-            if (seenChars.find(c) != seenChars.end()) {
-                seenChars.clear();
+        for (size_t right = 1; right < s.size(); ++right) {
+            for (size_t idx = left; idx < right; ++idx) {
+                if (s[idx] != s[right]) {
+                    ++count;
+                } else {
+                    left = idx + 1;
+                    break;
+                }
             }
-            seenChars.insert(c);
-            result = std::max(result, static_cast<int>(seenChars.size()));
+            result = std::max(result, count);
+            count = 1;
 
         }
         return result;
-
     }
 };
 
 
 int main() {
-     std::cout << "Solution for dvdf (3): " << Solution::lengthOfLongestSubstring("dvdf") << std::endl;
-
+    std::cout << "Solution for abcabcbb (3): " << Solution::lengthOfLongestSubstring("abcabcbb") << std::endl;
+    std::cout << "Solution for bbbbb (1): " << Solution::lengthOfLongestSubstring("bbbbb") << std::endl;
+    std::cout << "Solution for pwwkew (3): " << Solution::lengthOfLongestSubstring("pwwkew") << std::endl;
+    std::cout << "Solution for '' (0): " << Solution::lengthOfLongestSubstring("") << std::endl;
+    std::cout << "Solution for dvdf (3): " << Solution::lengthOfLongestSubstring("dvdf") << std::endl;
 }
