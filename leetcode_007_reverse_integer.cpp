2f4e90bbb504d264ebab4243713c083444af4435
Jochen Issing <c.333+github@nesono.com>
2021-05-04 01:20:06 +0200

    Add reverse integer

--- leetcode_007_reverse_integer.cpp
+++ leetcode_007_reverse_integer.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 
+#include "leetcode_000_common.h"
+
 class Solution {
 public:
-    int reverse(int x) {
+    static int reverse(int x) {
+        if (x == std::numeric_limits<int>::min()) {
+            return 0;
+        }
+        if (x < 0 ) {
+            return -reverse(-x);
+        }
 
+        int result = 0;
+        while (x > 0) {
+            int64_t tmp = static_cast<int64_t>(result) * 10LL + x % 10;
+            if (tmp > std::numeric_limits<int>::max()) {
+                return 0;
+            }
+            result = static_cast<int>(tmp);
+            x = x / 10;
+        }
+        return result;
     }
 };
 
 int main() {
-
+    testSolution(Solution::reverse, 123, 321);
+    testSolution(Solution::reverse, -123, -321);
+    testSolution(Solution::reverse, 0, 0);
+    testSolution(Solution::reverse,
+                 1534236469, 0);
+    testSolution(Solution::reverse, -2147483648, 0);
 }
